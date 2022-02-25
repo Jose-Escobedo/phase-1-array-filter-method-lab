4f806878848138d31d3eb06468f2e43595f63aaf
+ <+>
2022-02-25 13:50:22 -0800

    commit tree a6f9d57e0a064c8b8ed548813f10f4bb44b28662

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+function findMatching(arrDrivers, string) {
+  return arrDrivers.filter(function (e) {
+    return e.toUpperCase() == string.toUpperCase();
+  });
+}
+
+function fuzzyMatch(arrDrivers, string) {
+  return arrDrivers.filter(function (e) {
+    return e.startsWith("Sa");
+  });
+}
+
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(arrDrivers, string) {
+  return arrDrivers.filter(function (element) {
+    // console.log(element.name);
+    return element.name === string;
+  });
+}
+// console.log(matchName(drivers, "Bobby"));
+console.log(matchName(drivers, "Bobby"));
